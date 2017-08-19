2d80493e20d19869eaebbdfbc3e41cede43a970d
Raymond Boswel <raymondboswel@gmail.com>
2017-08-19 15:16:25 +0200

    Added guardian

--- mix.exs
+++ mix.exs
@@ -41,7 +41,8 @@ defmodule AmnesiaApi.Mixfile do
       {:phoenix_live_reload, "~> 1.0", only: :dev},
       {:gettext, "~> 0.11"},
       {:cowboy, "~> 1.0"},
-      {:cors_plug, "~> 1.2"}
+      {:cors_plug, "~> 1.2"},
+      {:guardian, "~> 0.14"}
     ]
   end
 
