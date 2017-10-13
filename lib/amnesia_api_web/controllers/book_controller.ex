7f82fb8340131f08219192ea34ee8ed010dfa21e
Raymond Boswel <raymondboswel@gmail.com>
2017-10-13 19:09:17 +0200

    Added controller method to add book with authors

--- lib/amnesia_api_web/controllers/book_controller.ex
+++ lib/amnesia_api_web/controllers/book_controller.ex
@@ -3,7 +3,10 @@ defmodule AmnesiaApiWeb.BookController do
 
   alias AmnesiaApi.Amnesia
   alias AmnesiaApi.Amnesia.Book
+  alias AmnesiaApi.Amnesia.BookAuthors
+  alias AmnesiaApi.Amnesia.Author
   import Ecto.Query
+  require Logger
 
   action_fallback AmnesiaApiWeb.FallbackController
 
@@ -15,6 +18,30 @@ defmodule AmnesiaApiWeb.BookController do
   def index(conn, _params) do
     books = Amnesia.list_books()
     render(conn, "index.json", books: books)
+  end
+
+  # Need to add or link authors if provided
+  def create(conn, %{"book" => %{"authors" => authors, "title" => title, "subtitle" => subtitle}} ) do
+    new_authors = Enum.filter(authors, fn(author) -> !Map.has_key?(author, "id") end)
+    existing_authors = Enum.filter(authors, fn(author) -> Map.has_key?(author, "id") end)
+    Enum.each(new_authors, fn(a) -> 
+        case AmnesiaApi.Repo.insert(AmnesiaApi.Amnesia.Author.changeset(%Author{}, a)) do
+          {:ok, author } -> [author | existing_authors]
+          {:error, changeset} -> Logger.error "Could not add author: #{inspect changeset}"        
+        end
+      end)
+    case Amnesia.create_book(%{title: title, subtitle: subtitle}) do
+      {:ok, book} -> 
+        Logger.warn "Book: #{inspect book}"
+        Enum.each(existing_authors, fn(author) -> AmnesiaApi.Repo.insert(%BookAuthors{book_id: book.id, author_id: author.id}) end)
+        conn 
+          |> put_status(:created)
+          |> put_resp_header("location", book_path(conn, :show, book))
+          |> render("show.json", book: book)
+      
+      {:error, changeset} ->  Logger.error "Could not add book: #{inspect changeset}"
+    end
+
   end
 
   def create(conn, %{"book" => book_params}) do
